df78652d927de597d54e4ed9fbaafac6fad7c0ba
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-10-18 20:24:27 +0530

    added Tailwind, ShadCN and configured both

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from "./components/ui/mode-toggle";
+
+export default function App() {
+  return (
+    <>
+      <ModeToggle>
+      </ModeToggle>
+      <Button variant="default">Button</Button>
+    </>
+  );
+}
